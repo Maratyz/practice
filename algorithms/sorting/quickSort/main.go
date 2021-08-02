45eefe2ddc3ca611353165b03872cfd88bb07a11
Marat <marat@datarepublic.com>
2021-08-02 23:10:45 +1000

    fix: duplicated elements are skipped

--- algorithms/sorting/quickSort/main.go
+++ algorithms/sorting/quickSort/main.go
@@ -5,8 +5,10 @@ import "fmt"
 func main() {
 	slice1 := []int{5,4,3,2,1}
 	slice2 := []int{8,9,10,7,6}
+	slice3 := []int{1,1,1,3,2}
 	fmt.Println(QuickSort(slice1))
 	fmt.Println(QuickSort(slice2))
+	fmt.Println(QuickSort(slice3))
 }
 
 func QuickSort( nums []int) []int {
@@ -16,16 +18,16 @@ func QuickSort( nums []int) []int {
 	pivot := nums[0]
 	var l, r []int
 
-	for i := 0 ; i < len(nums) ; i++ {
-		if nums[i] < pivot {
+	for i := 1 ; i < len(nums) ; i++ {
+		if nums[i] <= pivot {
 			l = append(l, nums[i])
 		}
 		if nums[i] > pivot {
 			r = append(r, nums[i])
 		}
 	}
-	// go does not have [] + [] syntax, append pivot to the left slice before recursion
-	l = append(l, pivot)
+	sorted := append(QuickSort(l), pivot)
+	sorted = append(sorted, QuickSort(r)...)
 
-	return append(QuickSort(l), QuickSort(r)...)
+	return sorted
 }
